64bac2132e750eaba303ceb58a9e4eb5303a43e3
Lucas Duarte Sobreira <lucasduartesobreira@gmail.com>
2021-07-06 05:10:42 -0300

    Removed rls as lsp server for now

--- lsp-servers/rust.lua
+++ lsp-servers/rust.lua
@@ -22,13 +22,18 @@ require'lspconfig'.rust_analyzer.setup {
   capabilities = capabilities,
   on_attach = on_attach
 }
-require'lspconfig'.rls.setup{
-	settings = {
-    rust = {
-      unstable_features = true,
-      build_on_save = false,
-      all_features = true,
-    },
-  },
-}
+--[[
+   [
+   [require'lspconfig'.rls.setup{
+   [  capabilities = capabilities,
+   [  on_attach =  on_attach,
+   [  settings = {
+   [    rust = {
+   [      unstable_features = true,
+   [      build_on_save = false,
+   [      all_features = true,
+   [    },
+   [  },
+   [}
+   ]]
 
