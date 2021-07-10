80acda7dd47e3974fead8c927ef3218ab8086c69
Lucas Duarte Sobreira <lucasduartesobreira@gmail.com>
2021-07-10 00:54:19 -0300

    style: Added commentaries to tab binds on compe-config

--- plug-config/compe-config.lua
+++ plug-config/compe-config.lua
@@ -1,47 +1,52 @@
 vim.o.completeopt = "menuone,noselect"
 
-require'compe'.setup {
-  enabled = true;
-  autocomplete = true;
-  debug = false;
-  min_length = 1;
-  preselect = 'enable';
-  throttle_time = 80;
-  source_timeout = 200;
-  resolve_timeout = 800;
-  incomplete_delay = 400;
-  max_abbr_width = 100;
-  max_kind_width = 100;
-  max_menu_width = 100;
+require "compe".setup {
+  enabled = true,
+  autocomplete = true,
+  debug = false,
+  min_length = 1,
+  preselect = "enable",
+  throttle_time = 80,
+  source_timeout = 200,
+  resolve_timeout = 800,
+  incomplete_delay = 400,
+  max_abbr_width = 100,
+  max_kind_width = 100,
+  max_menu_width = 100,
   documentation = {
-    border = { '', '' ,'', ' ', '', '', '', ' ' }, -- the border option is the same as `|help nvim_open_win|`
+    border = {"", "", "", " ", "", "", "", " "}, -- the border option is the same as `|help nvim_open_win|`
     winhighlight = "NormalFloat:CompeDocumentation,FloatBorder:CompeDocumentationBorder",
     max_width = 120,
     min_width = 60,
     max_height = math.floor(vim.o.lines * 0.3),
-    min_height = 1,
-  };
-
+    min_height = 1
+  },
   source = {
-    path = true;
-    buffer = true;
-    calc = true;
-    nvim_lsp = true;
-    nvim_lua = true;
-    ultisnips = true;
-    spell = true;
-    tags = true;
+    path = true,
+    buffer = true,
+    calc = true,
+    nvim_lsp = true,
+    nvim_lua = true,
+    ultisnips = true,
+    spell = true,
+    tags = true,
     --snippets_nvim = true;
-    treesitter = true;
-    tabnine = true;
-  };
+    treesitter = true,
+    tabnine = true
+  }
 }
 -- Default Keybinds
-vim.api.nvim_set_keymap("i","<C-Space>","compe#complete()", {expr = true, silent = true})
-vim.api.nvim_set_keymap("i", "<CR>", "compe#confirm({ 'keys': \"<Plug>delimitMateCR\", 'mode': '' })", {expr = true, silent = true})
+vim.api.nvim_set_keymap("i", "<C-Space>", "compe#complete()", {expr = true, silent = true})
+vim.api.nvim_set_keymap(
+  "i",
+  "<CR>",
+  'compe#confirm({ \'keys\': "<Plug>delimitMateCR", \'mode\': \'\' })',
+  {expr = true, silent = true}
+)
 vim.api.nvim_set_keymap("i", "<C-e>", "compe#close('<C-e>')", {expr = true, silent = true})
-vim.api.nvim_set_keymap("i","<C-f>","compe#scroll({ 'delta': +4 })", {expr = true, silent = true})
-vim.api.nvim_set_keymap("i","<C-d>","compe#scroll({ 'delta': -4 })", {expr = true, silent = true})
+vim.api.nvim_set_keymap("i", "<C-f>", "compe#scroll({ 'delta': +4 })", {expr = true, silent = true})
+vim.api.nvim_set_keymap("i", "<C-d>", "compe#scroll({ 'delta': -4 })", {expr = true, silent = true})
 
-vim.api.nvim_set_keymap("i","<Tab>","pumvisible() ? \"<C-n>\" : \"<Tab>\"", {expr = true})
-vim.api.nvim_set_keymap("i","<S-Tab>","pumvisible() ? \"<C-p>\" : \"<S-Tab>\"", {expr = true})
+-- Tab to move through completion menu
+vim.api.nvim_set_keymap("i", "<Tab>", 'pumvisible() ? "<C-n>" : "<Tab>"', {expr = true})
+vim.api.nvim_set_keymap("i", "<S-Tab>", 'pumvisible() ? "<C-p>" : "<S-Tab>"', {expr = true})
