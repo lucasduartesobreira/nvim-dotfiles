8f8a2d1914a05e11a1e31544db59ac4ccb60d64f
Lucas Duarte Sobreira <lucasduartesobreira@gmail.com>
2021-07-12 00:54:49 -0300

    fix: Fixed linting for go

--- lsp-servers/diagnosticls.lua
+++ lsp-servers/diagnosticls.lua
@@ -1,53 +1,53 @@
-require'lspconfig'.diagnosticls.setup{
-	filetypes = {"javascript", "typescript","go"},
-	init_options = {
-		linters = {
-			eslint = {
-				command = "./node_modules/.bin/eslint",
-				rootPatterns = {".git"},
-				debounce = 100,
-				args = {
-				    "--stdin",
-				    "--stdin-filename",
-				    "%filepath",
-				    "--format",
-				    "json"
-				},
-				sourceName = "eslint",
-				parseJson = {
-				    errorsRoot = "[0].messages",
-				    line = "line",
-				    column = "column",
-				    endLine = "endLine",
-				    endColumn = "endColumn",
-				    message = "${message} [${ruleId}]",
-				    security = "severity"
-				},
-				securities = {
-				    [2] = "error",
-				    [1] = "warning"
-				}
-			},
-			golang = {
-				command = "golangci-lint",
-				rootPatterns = { "go.mod" },
-				debounce = 100,
-				args = { "run", "--out-format", "json" },
-				sourceName = "golangci-lint",
-				parseJson = {
-						sourceName = "Pos.Filename",
-						sourceNameFilter = true,
-						errorsRoot = "Issues",
-						line = "Pos.Line",
-						column = "Pos.Column",
-						message = "${Text} [${FromLinter}]",
-				}
-		    }
-		},
-		filetypes = {
-			 javascript = "eslint",
-			 typescript = "eslint",
-			 go = "golangci-lint"
-		}
-	}
+require "lspconfig".diagnosticls.setup {
+  filetypes = {"javascript", "typescript", "go"},
+  init_options = {
+    linters = {
+      eslint = {
+        command = "eslint",
+        rootPatterns = {".git"},
+        debounce = 100,
+        args = {
+          "--stdin",
+          "--stdin-filename",
+          "%filepath",
+          "--format",
+          "json"
+        },
+        sourceName = "eslint",
+        parseJson = {
+          errorsRoot = "[0].messages",
+          line = "line",
+          column = "column",
+          endLine = "endLine",
+          endColumn = "endColumn",
+          message = "${message} [${ruleId}]",
+          security = "severity"
+        },
+        securities = {
+          [2] = "error",
+          [1] = "warning"
+        }
+      },
+      ["golangci-lint"] = {
+        command = "golangci-lint",
+        rootPatterns = {".git", "go.mod"},
+        debounce = 200,
+        args = {"run", "--enable", "golint", "--out-format", "json", "%filepath"},
+        sourceName = "golangci-lint",
+        parseJson = {
+          sourceName = "Pos.Filename",
+          sourceNameFilter = true,
+          errorsRoot = "Issues",
+          line = "Pos.Line",
+          column = "Pos.Column",
+          message = "${Text} [${FromLinter}]"
+        }
+      }
+    },
+    filetypes = {
+      javascript = "eslint",
+      typescript = "eslint",
+      go = "golangci-lint"
+    }
+  }
 }
