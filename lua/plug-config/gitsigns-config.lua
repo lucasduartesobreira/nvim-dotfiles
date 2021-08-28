31579223f1366cedf122d7d9ddaf0b6e1e107363
Lucas Duarte Sobreira <lucasduartesobreira@gmail.com>
2021-08-27 21:51:26 -0300

    fix: Removed some deprecated and unused options of gitsigns

--- lua/plug-config/gitsigns-config.lua
+++ lua/plug-config/gitsigns-config.lua
@@ -30,13 +30,9 @@ require("gitsigns").setup {
     interval = 1000,
     follow_files = true
   },
-  current_line_blame = false,
-  current_line_blame_delay = 1000,
-  current_line_blame_position = "eol",
   sign_priority = 6,
   update_debounce = 100,
   status_formatter = nil, -- Use default
   word_diff = false,
-  use_decoration_api = true,
   use_internal_diff = true -- If luajit is present
 }
