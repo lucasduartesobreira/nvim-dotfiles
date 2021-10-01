98a1d8a7f973b8d824fee55ba84e72eb5c9404b6
Lucas Duarte Sobreira <lucasduartesobreira@gmail.com>
2021-10-01 03:27:22 -0300

    refactor: Colors for lualine modes

--- lua/theme/github-theme.lua
+++ lua/theme/github-theme.lua
@@ -25,11 +25,21 @@ require "lualine".setup {
   tabline = {},
   extensions = {}
 }
+
 -- Lua
 require("github-theme").setup(
   {
     theme_style = "dark_default",
     function_style = "bold",
-    dark_float = true
+    dark_float = true,
+    colors = {
+      lualine = {
+        normal = {a = {bg = "blue", fg = "bg"}, b = {bg = "blue", fg = "bg"}, c = {bg = "blue", fg = "bg"}},
+        insert = {a = {bg = "green", fg = "bg"}, b = {bg = "green", fg = "bg"}, c = {bg = "green", fg = "bg"}},
+        command = {a = {bg = "magenta", fg = "bg"}, b = {bg = "magenta", fg = "bg"}, c = {bg = "magenta", fg = "bg"}},
+        visual = {a = {bg = "yellow", fg = "bg"}, b = {bg = "yellow", fg = "bg"}, c = {bg = "yellow", fg = "bg"}},
+        replace = {a = {bg = "red", fg = "bg"}, b = {bg = "red", fg = "bg"}, c = {bg = "red", fg = "bg"}}
+      }
+    }
   }
 )
