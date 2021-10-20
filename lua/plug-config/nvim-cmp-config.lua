0cf494e904b169f8b7866654eb35d2bc16bc459e
Lucas Duarte Sobreira <lucasduartesobreira@gmail.com>
2021-10-20 17:47:34 -0300

    fix(cmp): removed the confirm bind because of incompatibility with autopairs

--- lua/plug-config/nvim-cmp-config.lua
+++ lua/plug-config/nvim-cmp-config.lua
@@ -19,7 +19,6 @@ cmp.setup(
       ["<C-d>"] = cmp.mapping.scroll_docs(-4),
       ["<C-f>"] = cmp.mapping.scroll_docs(4),
       ["<C-Space>"] = cmp.mapping.complete(),
-      --["<CR>"] = cmp.mapping.confirm({select = true}), removed because of autopairs
       ["<C-y>"] = cmp.mapping.close(),
       ["<Tab>"] = cmp.mapping(
         function(fallback)
