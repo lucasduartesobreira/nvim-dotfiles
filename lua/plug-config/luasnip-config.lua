7ffaf2efb9f53075ed41f8ff2134251d130680cb
Lucas Duarte Sobreira <lucasduartesobreira@gmail.com>
2021-10-20 17:54:56 -0300

    fix(luasnip):made the binds for the choice cleaner

--- lua/plug-config/luasnip-config.lua
+++ lua/plug-config/luasnip-config.lua
@@ -25,16 +25,5 @@ ls.snippets = {
 }
 
 require("luasnip/loaders/from_vscode").lazy_load()
-
-vim.api.nvim_set_keymap(
-  "i",
-  "<C-E>",
-  "luasnip#choice_active() ? '<Plug>luasnip-next-choice' : '<C-E>'",
-  {expr = true, silent = true}
-)
-vim.api.nvim_set_keymap(
-  "s",
-  "<C-E>",
-  "luasnip#choice_active() ? '<Plug>luasnip-next-choice' : '<C-E>'",
-  {expr = true, silent = true}
-)
+vim.api.nvim_set_keymap("i", "<C-E>", "<Plug>luasnip-next-choice", {expr = true, silent = true})
+vim.api.nvim_set_keymap("s", "<C-E>", "<Plug>luasnip-next-choice", {expr = true, silent = true})
