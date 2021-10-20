14757c7e7efbfb7a5d5c6ec3a6cb2ad4738ac2ae
Lucas Duarte Sobreira <lucasduartesobreira@gmail.com>
2021-10-20 17:44:51 -0300

    feat(cmp): added C-n and C-p binds to go through the list

--- lua/plug-config/nvim-cmp-config.lua
+++ lua/plug-config/nvim-cmp-config.lua
@@ -14,6 +14,8 @@ cmp.setup(
       end
     },
     mapping = {
+      ["<C-n>"] = cmp.mapping.select_next_item({behavior = cmp.SelectBehavior.Insert}),
+      ["<C-p>"] = cmp.mapping.select_prev_item({behavior = cmp.SelectBehavior.Insert}),
       ["<C-d>"] = cmp.mapping.scroll_docs(-4),
       ["<C-f>"] = cmp.mapping.scroll_docs(4),
       ["<C-Space>"] = cmp.mapping.complete(),
