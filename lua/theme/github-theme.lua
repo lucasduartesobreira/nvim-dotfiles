31241dd5a8c30ae5c36db7cfc116171b252ed47f
Lucas Duarte Sobreira <lucasduartesobreira@gmail.com>
2021-12-02 17:11:03 -0300

    fix(github-theme): back to default theme config

--- lua/theme/github-theme.lua
+++ lua/theme/github-theme.lua
@@ -2,8 +2,6 @@ require "lualine".setup {
   options = {
     icons_enabled = true,
     theme = "github",
-    component_separators = "",
-    section_separators = "",
     disabled_filetypes = {}
   },
   sections = {
@@ -32,14 +30,5 @@ require("github-theme").setup(
     theme_style = "dark_default",
     function_style = "bold",
     dark_float = true,
-    colors = {
-      lualine = {
-        normal = {a = {bg = "blue", fg = "bg"}, b = {bg = "blue", fg = "bg"}, c = {bg = "blue", fg = "bg"}},
-        insert = {a = {bg = "green", fg = "bg"}, b = {bg = "green", fg = "bg"}, c = {bg = "green", fg = "bg"}},
-        command = {a = {bg = "magenta", fg = "bg"}, b = {bg = "magenta", fg = "bg"}, c = {bg = "magenta", fg = "bg"}},
-        visual = {a = {bg = "yellow", fg = "bg"}, b = {bg = "yellow", fg = "bg"}, c = {bg = "yellow", fg = "bg"}},
-        replace = {a = {bg = "red", fg = "bg"}, b = {bg = "red", fg = "bg"}, c = {bg = "red", fg = "bg"}}
-      }
-    }
   }
 )
