56cd92f53ffbc762976288e8f669b9e23c062e5c
Lucas Duarte Sobreira <lucasduartesobreira@gmail.com>
2022-04-02 14:28:16 -0300

    fix(tsserver): disable auto imports because it makes completion unusable

--- lsp-servers/tsserver.lua
+++ lsp-servers/tsserver.lua
@@ -6,5 +6,20 @@ local on_attach = require "plug-config/lspsignature-config".on_attach
 
 require "lspconfig".tsserver.setup {
   capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities),
-  on_attach = on_attach
+  on_attach = function(client, bufnr)
+    client.resolved_capabilities.document_formatting = false
+    on_attach(client, bufnr)
+  end,
+  init_options = {
+    hostInfo = "neovim",
+    disableAutomaticTypingAcquisition = false,
+    preferences = {
+      allowIncompleteCompletions = true,
+      includeCompletionsForModuleExports = false
+    }
+  },
+  flags = {
+    debounce_text_changes = 500,
+    allow_incremental_sync = true
+  }
 }
