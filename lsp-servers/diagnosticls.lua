52340727098eefe78d5d9bcb6e42a318b3882b60
Lucas Duarte Sobreira <lucasduartesobreira@gmail.com>
2022-04-03 20:27:46 -0300

    feat(diagnosticls): add formatters and linters to diagnosticsls

--- lsp-servers/diagnosticls.lua
+++ lsp-servers/diagnosticls.lua
@@ -1,54 +1,140 @@
+local linters = {
+  eslint_d = {
+    command = "eslint_d",
+    rootPatterns = {
+      ".eslintrc",
+      ".eslintrc.cjs",
+      ".eslintrc.js",
+      ".eslintrc.json",
+      ".eslintrc.yaml",
+      ".eslintrc.yml"
+    },
+    debounce = 100,
+    args = {
+      "--stdin",
+      "--stdin-filename",
+      "%filepath",
+      "--format",
+      "json"
+    },
+    sourceName = "eslint",
+    parseJson = {
+      errorsRoot = "[0].messages",
+      line = "line",
+      column = "column",
+      endLine = "endLine",
+      endColumn = "endColumn",
+      message = "${message} [${ruleId}]",
+      security = "severity"
+    },
+    securities = {
+      [2] = "error",
+      [1] = "warning"
+    }
+  },
+  golangci_lint = {
+    command = "golangci-lint",
+    args = {"run", "--out-format", "json"},
+    debounce = 100,
+    parseJson = {
+      sourceNameFilter = true,
+      sourceName = "Pos.Filename",
+      errorsRoot = "Issues",
+      line = "Pos.Line",
+      column = "Pos.Column",
+      message = "[golangci_lint] ${Text} [${FromLinter}]"
+    },
+    rootPatterns = {".git", "go.mod"}
+  },
+  standard = {
+    command = "./node_modules/.bin/standard",
+    debounce = 100,
+    args = {"--stdin"},
+    offsetLine = 0,
+    offsetColumn = 0,
+    formatLines = 1,
+    formatPattern = {
+      [[^\s*<\w+>:(\d+):(\d+):\s+(.*)(\r|\n)*$]],
+      {line = 1, column = 2, message = {"[standard] ", 3}}
+    },
+    rootPatterns = {".git"}
+  }
+}
+
+local formatters = {
+  prettier = {
+    command = "prettier",
+    args = {"--stdin", "--stdin-filepath", "%filepath"},
+    rootPatterns = {
+      ".prettierrc",
+      ".prettierrc.json",
+      ".prettierrc.toml",
+      ".prettierrc.json",
+      ".prettierrc.yml",
+      ".prettierrc.yaml",
+      ".prettierrc.json5",
+      ".prettierrc.js",
+      ".prettierrc.cjs",
+      "prettier.config.js",
+      "prettier.config.cjs"
+    }
+  },
+  standard_fmt = {
+    command = "./node_modules/.bin/standard",
+    args = {"--stdin", "--fix"},
+    rootPatterns = {".git"},
+    isStderr = false,
+    isStdout = true
+  },
+  eslint_d_fmt = {
+    command = "eslint_d",
+    args = {
+      "--fix-to-stdout",
+      "--stdin",
+      "--stdin-filename",
+      "%filepath"
+    },
+    rootPatterns = {
+      ".eslintrc",
+      ".eslintrc.cjs",
+      ".eslintrc.js",
+      ".eslintrc.json",
+      ".eslintrc.yaml",
+      ".eslintrc.yml"
+    }
+  },
+  luafmt = {
+    command = "luafmt",
+    args = {"--indent-count", 2, "--stdin"}
+  },
+  gofmt = {
+    command = "gofmt",
+    args = {"-s"},
+    rootPatterns = {"go.mod", ".git"}
+  },
+  goimports = {
+    command = "goimports",
+    rootPatterns = {"go.mod", ".git"}
+  }
+}
+
 require "lspconfig".diagnosticls.setup {
-  filetypes = {"javascript", "typescript", "go"},
+  filetypes = {"javascript", "typescript", "go", "lua"},
   cmd = {"diagnostic-languageserver", "--stdio", "--log-level", "2"},
   init_options = {
-    linters = {
-      eslint = {
-        command = "eslint",
-        rootPatterns = {".git"},
-        debounce = 100,
-        args = {
-          "--stdin",
-          "--stdin-filename",
-          "%filepath",
-          "--format",
-          "json"
-        },
-        sourceName = "eslint",
-        parseJson = {
-          errorsRoot = "[0].messages",
-          line = "line",
-          column = "column",
-          endLine = "endLine",
-          endColumn = "endColumn",
-          message = "${message} [${ruleId}]",
-          security = "severity"
-        },
-        securities = {
-          [2] = "error",
-          [1] = "warning"
-        }
-      },
-      ["golangci-lint"] = {
-        command = "golangci-lint",
-        rootPatterns = {".git", "go.mod"},
-        debounce = 100,
-        args = {"run", "--out-format", "json", "%filepath"},
-        sourceName = "golangci-lint",
-        parseJson = {
-          sourceName = "Pos.Filename",
-          sourceNameFilter = true,
-          errorsRoot = "Issues",
-          line = "Pos.Line",
-          column = "Pos.Column",
-          message = "${Text} [${FromLinter}]"
-        }
-      }
-    },
+    linters = linters,
     filetypes = {
-      javascript = "eslint",
-      typescript = "eslint",
-      go = "golangci-lint"
+      javascript = "eslint_d",
+      typescript = "eslint_d",
+      go = "golangci_lint"
+    },
+    formatters = formatters,
+    formatFiletypes = {
+      typescript = {"standard_fmt", "eslint_d_fmt", "prettier"},
+      javascript = {"standard_fmt", "eslint_d_fmt", "prettier"},
+      rust = {"rustfmt"},
+      lua = {"luafmt"},
+      go = {"gofmt", "goimports"}
     }
   }
 }
