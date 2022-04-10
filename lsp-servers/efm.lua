a5e795bdbba811c9bc402b91b2ab3758b4576ac7
Lucas Duarte Sobreira <lucasduartesobreira@gmail.com>
2022-04-10 01:20:52 -0300

    feat(lsp): add efmls to replace diagnosticsls

---
+++ lsp-servers/efm.lua
@@ -0,0 +1,16 @@
+local lsp_format_attach = require("lsp-format").on_attach
+
+function on_attach(client, bufnr)
+  lsp_format_attach(client)
+end
+
+local home = vim.fn.expand("$HOME")
+local config_root = home .. "/.config/nvim/lsp-config"
+local config = config_root .. "/config.yaml"
+
+require "lspconfig".efm.setup {
+  on_attach = on_attach,
+  filetypes = {"javascript", "typescript", "go", "lua"},
+  cmd = {"efm-langserver", "-c", config},
+  init_options = {documentFormatting = true, publishDiagnostics = true}
+}
--- lsp-servers/setup-all.vim
+++ lsp-servers/setup-all.vim
@@ -2,6 +2,7 @@ luafile $HOME/.config/nvim/lsp-servers/go.lua
 luafile $HOME/.config/nvim/lsp-servers/rust.lua
 luafile $HOME/.config/nvim/lsp-servers/lua.lua
 luafile $HOME/.config/nvim/lsp-servers/tsserver.lua
-luafile $HOME/.config/nvim/lsp-servers/diagnosticls.lua
+"luafile $HOME/.config/nvim/lsp-servers/diagnosticls.lua
 "luafile $HOME/.config/nvim/lsp-servers/ocaml.lua
 luafile $HOME/.config/nvim/lsp-servers/python.lua
+luafile $HOME/.config/nvim/lsp-servers/efm.lua
