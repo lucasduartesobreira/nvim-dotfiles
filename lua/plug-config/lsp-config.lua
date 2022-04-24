305fb3b4e7834876a20b33e77b7c3214a965b75e
Lucas Duarte Sobreira <lucasduartesobreira@gmail.com>
2022-04-24 14:37:51 -0300

    remove a lot of unused files

--- lua/plug-config/lsp-config.lua
+++
@@ -1,9 +0,0 @@
-vim.lsp.handlers["textDocument/publishDiagnostics"] =
-  vim.lsp.with(
-  vim.lsp.diagnostic.on_publish_diagnostics,
-  {
-    signs = true,
-    virtual_text = false,
-    underline = true
-  }
-)
