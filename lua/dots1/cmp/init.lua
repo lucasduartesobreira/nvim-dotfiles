eb1954e0c394ad5d175826544333b13a8bf7a527
Lucas Duarte Sobreira <lucasduartesobreira@gmail.com>
2022-04-24 12:37:50 -0300

    style: run formatter

--- lua/dots1/cmp/init.lua
+++ lua/dots1/cmp/init.lua
@@ -1,9 +1,9 @@
-local status_cmp_ok, cmp = pcall(require, 'cmp')
+local status_cmp_ok, cmp = pcall(require, "cmp")
 if not status_cmp_ok then
   return
 end
 
-local status_luasnip_ok, luasnip = pcall(require, 'luasnip')
+local status_luasnip_ok, luasnip = pcall(require, "luasnip")
 if not status_luasnip_ok then
   return
 end
@@ -13,96 +13,113 @@ local has_words_before = function()
   return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
 end
 
-cmp.setup({
-  snippet = {
-    expand = function(args)
-      luasnip.lsp_expand(args.body)
-    end,
-  },
-  window = {
-    completion = cmp.config.window.bordered(),
-    documentation = cmp.config.window.bordered(),
-  },
-  mapping = cmp.mapping.preset.insert({
-    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping.complete(),
-    ["<C-y>"] = cmp.mapping.close(),
-    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-    ["<Tab>"] = cmp.mapping(
-      function(fallback)
-        if cmp.visible() then
-          cmp.select_next_item()
-        elseif luasnip.expand_or_jumpable() then
-          luasnip.expand_or_jump()
-        elseif has_words_before() then
-          cmp.complete()
-        else
-          fallback()
-        end
-      end,
-      { "i", "s" }
-    ),
-    ["<S-Tab>"] = cmp.mapping(
-      function(fallback)
-        if cmp.visible() then
-          cmp.select_prev_item()
-        elseif luasnip.jumpable(-1) then
-          luasnip.jump(-1)
-        else
-          fallback()
-        end
-      end,
-      { "i", "s" }
-    )
-  }),
-  sources = cmp.config.sources({
-    { name = 'nvim_lsp' },
-    { name = 'luasnip' },
-    { name = 'cmp_tabnine' },
-    { name = 'nvim_lsp_signature_help' }
-  }, {
-    { name = 'buffer' },
-    { name = 'path' },
-    { name = 'spell' },
-  }, {
-    { name = 'emoji' },
-    { name = 'latex_symbols' }
-  }),
-  formatting = {
-    format = require("lspkind").cmp_format(
+cmp.setup(
+  {
+    snippet = {
+      expand = function(args)
+        luasnip.lsp_expand(args.body)
+      end
+    },
+    window = {
+      completion = cmp.config.window.bordered(),
+      documentation = cmp.config.window.bordered()
+    },
+    mapping = cmp.mapping.preset.insert(
       {
-        with_text = true,
-        menu = ({
-          buffer = "[Buffer]",
-          path = "[Path]",
-          spell = "[Spell]",
-          nvim_lsp = "[LSP]",
-          luasnip = "[LuaSnip]",
-          nvim_lua = "[Lua]",
-          latex_symbols = "[Latex]",
-          cmp_tabnine = "[TabNine]",
-          emoji = "[Emoji]"
-        })
+        ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-f>"] = cmp.mapping.scroll_docs(4),
+        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<C-y>"] = cmp.mapping.close(),
+        ["<CR>"] = cmp.mapping.confirm({select = true}), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+        ["<Tab>"] = cmp.mapping(
+          function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            elseif luasnip.expand_or_jumpable() then
+              luasnip.expand_or_jump()
+            elseif has_words_before() then
+              cmp.complete()
+            else
+              fallback()
+            end
+          end,
+          {"i", "s"}
+        ),
+        ["<S-Tab>"] = cmp.mapping(
+          function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+              luasnip.jump(-1)
+            else
+              fallback()
+            end
+          end,
+          {"i", "s"}
+        )
+      }
+    ),
+    sources = cmp.config.sources(
+      {
+        {name = "nvim_lsp"},
+        {name = "luasnip", max_item_count = 3},
+        {name = "cmp_tabnine", max_item_count = 3},
+        {name = "nvim_lsp_signature_help"}
+      },
+      {
+        {name = "buffer", max_item_count = 2},
+        {name = "path", max_item_count = 4},
+        {name = "spell", max_item_count = 2}
+      },
+      {
+        {name = "emoji", max_item_count = 4},
+        {name = "latex_symbols", max_item_count = 4}
+      }
+    ),
+    formatting = {
+      format = require("lspkind").cmp_format(
+        {
+          with_text = true,
+          menu = ({
+            buffer = "[Buffer]",
+            path = "[Path]",
+            spell = "[Spell]",
+            nvim_lsp = "[LSP]",
+            luasnip = "[LuaSnip]",
+            nvim_lua = "[Lua]",
+            latex_symbols = "[Latex]",
+            cmp_tabnine = "[TabNine]",
+            emoji = "[Emoji]"
+          })
+        }
+      )
+    }
+  }
+)
+
+-- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline(
+  "/",
+  {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = {
+      {name = "buffer"}
+    }
+  }
+)
+
+-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline(
+  ":",
+  {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = cmp.config.sources(
+      {
+        {name = "path"}
+      },
+      {
+        {name = "cmdline"}
       }
     )
   }
-})
-
--- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
-cmp.setup.cmdline('/', {
-  mapping = cmp.mapping.preset.cmdline(),
-  sources = {
-    { name = 'buffer' }
-  }
-})
-
--- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-cmp.setup.cmdline(':', {
-  mapping = cmp.mapping.preset.cmdline(),
-  sources = cmp.config.sources({
-    { name = 'path' }
-  }, {
-    { name = 'cmdline' }
-  })
-})
+)
--- lua/dots1/telescope/init.lua
+++ lua/dots1/telescope/init.lua
@@ -2,43 +2,46 @@ local status_telescope_ok, telescope = pcall(require, 'telescope')
 if not status_telescope_ok then
   return
 end
+
 telescope.setup{
-defaults = {
+  defaults = {
     vimgrep_arguments = {
-        'rg',
-        '--color=never',
-        '--no-heading',
-        '--with-filename',
-        '--line-number',
-        '--column',
-        '--smart-case'
-        },
+      'rg',
+      '--color=never',
+      '--no-heading',
+      '--with-filename',
+      '--line-number',
+      '--column',
+      '--smart-case'
+    },
     prompt_prefix = "> ",
     selection_caret = "> ",
     color_devicons = true,
     layout_strategy = "flex",
     layout_config = {
-        horizontal = {
-            mirror = false,
-            },
-        vertical = {
-            mirror = false,
-            },
-        preview_cutoff = 0
-        },
+      horizontal = {
+        mirror = false,
+      },
+      vertical = {
+        mirror = false,
+      },
+      preview_cutoff = 0
     },
-extensions = {
+    file_previewer = require("telescope.previewers").vim_buffer_cat.new,
+    grep_previewer = require("telescope.previewers").vim_buffer_vimgrep.new,
+  },
+  extensions = {
     fzf = {
-        fuzzy = true,                    -- false will only do exact matching
-        override_generic_sorter = false, -- override the generic sorter
-        override_file_sorter = true,     -- override the file sorter
-        case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
-        -- the default case_mode is "smart_case"
-        },
-    }
+      fuzzy = true,                    -- false will only do exact matching
+      override_generic_sorter = false, -- override the generic sorter
+      override_file_sorter = true,     -- override the file sorter
+      case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
+      -- the default case_mode is "smart_case"
+    },
+  }
 }
 
 telescope.load_extension('fzf')
-telescope.load_extension "file_browser"
+telescope.load_extension( "file_browser" )
 
 require("dots1.telescope.mappings")
