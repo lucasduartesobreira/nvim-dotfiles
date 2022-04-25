5e6624c471bb9800e9ea647d8afb7dc586747d21
Lucas Duarte Sobreira <lucasduartesobreira@gmail.com>
2022-04-24 21:00:26 -0300

    fix(telescope): bind C-n and C-p to move previous and next

--- lua/dots1/telescope/init.lua
+++ lua/dots1/telescope/init.lua
@@ -1,18 +1,20 @@
-local status_telescope_ok, telescope = pcall(require, 'telescope')
+local status_telescope_ok, telescope = pcall(require, "telescope")
 if not status_telescope_ok then
   return
 end
 
-telescope.setup{
+local actions = require("telescope.actions")
+
+telescope.setup {
   defaults = {
     vimgrep_arguments = {
-      'rg',
-      '--color=never',
-      '--no-heading',
-      '--with-filename',
-      '--line-number',
-      '--column',
-      '--smart-case'
+      "rg",
+      "--color=never",
+      "--no-heading",
+      "--with-filename",
+      "--line-number",
+      "--column",
+      "--smart-case"
     },
     prompt_prefix = "> ",
     selection_caret = "> ",
@@ -20,28 +22,35 @@ telescope.setup{
     layout_strategy = "flex",
     layout_config = {
       horizontal = {
-        mirror = false,
+        mirror = false
       },
       vertical = {
-        mirror = false,
+        mirror = false
       },
       preview_cutoff = 0
     },
     file_previewer = require("telescope.previewers").vim_buffer_cat.new,
     grep_previewer = require("telescope.previewers").vim_buffer_vimgrep.new,
+    mappings = {
+      i = {
+        ["<C-\\>"] = actions.which_key,
+        ["<C-n>"] = actions.move_selection_previous,
+        ["<C-p>"] = actions.move_selection_next
+      }
+    }
   },
   extensions = {
     fzf = {
-      fuzzy = true,                    -- false will only do exact matching
+      fuzzy = true, -- false will only do exact matching
       override_generic_sorter = false, -- override the generic sorter
-      override_file_sorter = true,     -- override the file sorter
-      case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
+      override_file_sorter = true, -- override the file sorter
+      case_mode = "smart_case" -- or "ignore_case" or "respect_case"
       -- the default case_mode is "smart_case"
-    },
+    }
   }
 }
 
-telescope.load_extension('fzf')
-telescope.load_extension( "file_browser" )
+telescope.load_extension("fzf")
+telescope.load_extension("file_browser")
 
 require("dots1.telescope.mappings")
