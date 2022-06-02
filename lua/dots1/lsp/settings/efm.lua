d98cd799c5fa0087274ea52e22827c8d1009ba4b
Lucas Duarte Sobreira <lucasduartesobreira@gmail.com>
2022-06-01 22:20:36 -0300

    feat(lsp): create autocmd to stop eslint_d server before close vim

--- lua/dots1/lsp/settings/efm.lua
+++ lua/dots1/lsp/settings/efm.lua
@@ -3,7 +3,21 @@ local config_root = home .. "/.config/nvim/lsp-config"
 local efm_config_path = config_root .. "/config.yaml"
 
 return {
-  --on_attach = on_attach,
+  on_attach = function(client, bufnr)
+    local augroup = vim.api.nvim_create_augroup
+    local aucmd = vim.api.nvim_create_autocmd
+    local group = augroup("Eslint_dStop", {})
+    aucmd(
+      {"VimLeavePre"},
+      {
+        group = group,
+        pattern = {"*.ts"},
+        callback = function()
+          os.execute("eslint_d stop")
+        end
+      }
+    )
+  end,
   filetypes = {"javascript", "typescript", "go", "lua"},
   cmd = {"efm-langserver", "-c", efm_config_path},
   init_options = {documentFormatting = true, publishDiagnostics = true}
