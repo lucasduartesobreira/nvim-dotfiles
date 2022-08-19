9b69c4c9fb4c3e3d9ae8cbb559cf197a285af652
Lucas Duarte Sobreira <lucasduartesobreira@gmail.com>
2022-08-18 22:16:26 -0300

    style(dap): apply formatting

--- lua/dots1/dap/dap_visual.lua
+++ lua/dots1/dap/dap_visual.lua
@@ -1,54 +1,76 @@
-local is_dap_ok, dap = pcall(require, 'dap')
+local is_dap_ok, dap = pcall(require, "dap")
 if not is_dap_ok then
   return
 end
 
-local is_dapui_ok, dapui = pcall(require, 'dapui')
+local is_dapui_ok, dapui = pcall(require, "dapui")
 if not is_dapui_ok then
   return
 end
 
 dapui.setup(
   {
-    icons = { expanded = "▾", collapsed = "▸" },
+    icons = {expanded = "▾", collapsed = "▸"},
     mappings = {
       -- Use a table to apply multiple mappings
-      expand = { "<CR>", "<2-LeftMouse>" },
+      expand = {"<CR>", "<2-LeftMouse>"},
       open = "o",
       remove = "d",
       edit = "e",
       repl = "r",
       toggle = "t"
     },
-    sidebar = {
-      -- You can change the order of elements in the sidebar
-      elements = {
-        -- Provide as ID strings or tables with "id" and "size" keys
-        {
-          id = "scopes",
-          size = 0.25 -- Can be float or integer > 1
+    layouts = {
+      {
+        elements = {
+          "scopes",
+          "breakpoints",
+          "stacks",
+          "watches"
         },
-        { id = "breakpoints", size = 0.25 },
-        { id = "stacks", size = 0.25 },
-        { id = "watches", size = 0.25 }
+        size = 40,
+        position = "left"
       },
-      size = 40,
-      position = "left" -- Can be "left", "right", "top", "bottom"
+      {
+        elements = {
+          "repl",
+          "console"
+        },
+        size = 10,
+        position = "bottom"
+      }
     },
-    tray = {
-      elements = { "repl" },
-      size = 10,
-      position = "bottom" -- Can be "left", "right", "top", "bottom"
-    },
+    --[[
+       [sidebar = {
+       [  -- You can change the order of elements in the sidebar
+       [  elements = {
+       [    -- Provide as ID strings or tables with "id" and "size" keys
+       [    {
+       [      id = "scopes",
+       [      size = 0.25 -- Can be float or integer > 1
+       [    },
+       [    { id = "breakpoints", size = 0.25 },
+       [    { id = "stacks", size = 0.25 },
+       [    { id = "watches", size = 0.25 }
+       [  },
+       [  size = 40,
+       [  position = "left" -- Can be "left", "right", "top", "bottom"
+       [},
+       [tray = {
+       [  elements = { "repl" },
+       [  size = 10,
+       [  position = "bottom" -- Can be "left", "right", "top", "bottom"
+       [},
+       ]]
     floating = {
       max_height = nil, -- These can be integers or a float between 0 and 1.
       max_width = nil, -- Floats will be treated as percentage of your screen.
       border = "rounded", -- Border style. Can be "single", "double" or "rounded"
       mappings = {
-        close = { "q", "<Esc>" }
+        close = {"q", "<Esc>"}
       }
     },
-    windows = { indent = 1 },
+    windows = {indent = 1},
     render = {
       max_type_length = nil -- Can be integer or nil.
     }
@@ -78,7 +100,7 @@ dapvt.setup {
   show_stop_reason = true, -- show stop reason when stopped for exceptions
   commented = false, -- prefix virtual text with comment string
   -- experimental features:
-  virt_text_pos = 'eol', -- position of virtual text, see `:h nvim_buf_set_extmark()`
+  virt_text_pos = "eol", -- position of virtual text, see `:h nvim_buf_set_extmark()`
   all_frames = false, -- show virtual text for all stack frames not only current. Only works for debugpy on my machine.
   virt_lines = false, -- show virtual lines instead of virtual text (will flicker!)
   virt_text_win_col = nil -- position the virtual text at a fixed window column (starting from the first text column) ,
