abebdf2a6582a13351e55a7d64c5a20ec4fe2530
Lucas Duarte Sobreira <lucasduartesobreira@gmail.com>
2022-08-26 21:18:17 -0300

    fix(init): always load plugins first

--- init.lua
+++ init.lua
@@ -1,7 +1,7 @@
+require("dots1.plugins")
 require("dots1.theme")
 require("dots1.options")
 require("dots1.keybinds")
-require("dots1.plugins")
 require("dots1.cmp")
 require("dots1.telescope")
 require("dots1.treesitter")
