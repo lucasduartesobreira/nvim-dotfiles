f99d8da16e3c2bdc67751e8a37c19561a01a8e35
Lucas Duarte Sobreira <lucasduartesobreira@gmail.com>
2022-08-28 21:11:24 -0300

    refactor(lualine): remove useless info from lualine and remove separators

--- lua/dots1/lualine/init.lua
+++ lua/dots1/lualine/init.lua
@@ -2,13 +2,15 @@ require "lualine".setup {
   options = {
     icons_enabled = true,
     theme = "nightfox",
-    disabled_filetypes = {}
+    disabled_filetypes = {},
+    section_separators = "",
+    component_separators = ""
   },
   sections = {
     lualine_a = {"mode"},
     lualine_b = {"branch", "diff"},
     lualine_c = {"filename"},
-    lualine_x = {"encoding", "fileformat", "filetype"},
+    lualine_x = {"filetype"},
     lualine_y = {"progress"},
     lualine_z = {"location"}
   },
