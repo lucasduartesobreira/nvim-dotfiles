2663ed1f8bb9e5ee047a78f2798233c44ef5e8e5
Lucas Duarte Sobreira <lucasduartesobreira@gmail.com>
2022-08-31 15:34:49 -0300

    feat(🔭): open all selected files

--- lua/dots1/telescope/init.lua
+++ lua/dots1/telescope/init.lua
@@ -4,6 +4,32 @@ if not status_telescope_ok then
 end
 
 local actions = require("telescope.actions")
+local action_state = require("telescope.actions.state")
+
+local telescope_custom_actions = {}
+
+function telescope_custom_actions._multiopen(prompt_bufnr, open_cmd)
+  local picker = action_state.get_current_picker(prompt_bufnr)
+  local selected_entry = action_state.get_selected_entry()
+  local num_selections = #picker:get_multi_selection()
+  if not num_selections or num_selections <= 1 then
+    actions.add_selection(prompt_bufnr)
+  end
+  actions.send_selected_to_qflist(prompt_bufnr)
+  vim.cmd("cfdo " .. open_cmd)
+end
+function telescope_custom_actions.multi_selection_open_vsplit(prompt_bufnr)
+  telescope_custom_actions._multiopen(prompt_bufnr, "vsplit")
+end
+function telescope_custom_actions.multi_selection_open_split(prompt_bufnr)
+  telescope_custom_actions._multiopen(prompt_bufnr, "split")
+end
+function telescope_custom_actions.multi_selection_open_tab(prompt_bufnr)
+  telescope_custom_actions._multiopen(prompt_bufnr, "tabe")
+end
+function telescope_custom_actions.multi_selection_open(prompt_bufnr)
+  telescope_custom_actions._multiopen(prompt_bufnr, "edit")
+end
 
 telescope.setup {
   defaults = {
@@ -35,7 +61,11 @@ telescope.setup {
       i = {
         ["<C-\\>"] = actions.which_key,
         ["<C-n>"] = actions.move_selection_previous,
-        ["<C-p>"] = actions.move_selection_next
+        ["<C-p>"] = actions.move_selection_next,
+        ["<CR>"] = telescope_custom_actions.multi_selection_open,
+        ["<C-V>"] = telescope_custom_actions.multi_selection_open_vsplit,
+        ["<C-S>"] = telescope_custom_actions.multi_selection_open_split,
+        ["<C-T>"] = telescope_custom_actions.multi_selection_open_tab
       }
     }
   },
