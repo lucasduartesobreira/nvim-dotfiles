73f94e31f227def6a27681fbc9a15b4699a522a4
Lucas Duarte Sobreira <lucasduartesobreira@gmail.com>
2022-09-15 23:31:09 -0300

    feat(binds): paste and keep the paste content

--- lua/dots1/keybinds.lua
+++ lua/dots1/keybinds.lua
@@ -1,11 +1,13 @@
 local opts = {noremap = true, silent = true}
 
-local keymap = vim.api.nvim_set_keymap
+local keymap = vim.keymap.set
 
 -- Map leader
 keymap("n", "<Space>", "<Nop>", opts)
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
+
+keymap({"n", "v"}, "<leader>p", '"_dP')
 
 -- move through windows
 keymap("n", "<C-h>", "<C-w>h", opts)
