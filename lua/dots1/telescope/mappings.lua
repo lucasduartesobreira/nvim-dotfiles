0dd962de0951de5e8f573a7d488c204fd6129d04
Lucas Duarte Sobreira <lucasduartesobreira@gmail.com>
2022-09-24 02:33:46 -0300

    fix(telescope): bind using keymap and loading once telescope

--- lua/dots1/telescope/mappings.lua
+++ lua/dots1/telescope/mappings.lua
@@ -1,20 +1,142 @@
 local opts = {noremap = true, silent = true}
-local keymap = vim.api.nvim_set_keymap
+local keymap = vim.keymap.set
 
-keymap("n", "<leader>ff", "<cmd>lua require('telescope.builtin').find_files()<CR>", opts)
-keymap("n", "<leader>fgf", "<cmd>lua require('telescope.builtin').git_files()<cr>", opts)
-keymap("n", "<leader>fw", "<cmd>lua require('telescope.builtin').live_grep()<cr>", opts)
-keymap("n", "<leader>bfw", "<cmd>lua require('telescope.builtin').current_buffer_fuzzy_find()<cr>", opts)
-keymap("n", "<leader>*", "<cmd>lua require('telescope.builtin').grep_string()<cr>", opts)
-keymap("n", "<leader>**", "<cmd>lua require('telescope.builtin').grep_string({ grep_open_files = true })<cr>", opts)
-keymap("n", "<leader>lb", "<cmd>lua require('telescope.builtin').buffers()<cr>", opts)
-keymap("n", "<leader>lj", "<cmd>lua require('telescope.builtin').jumplist()<cr>", opts)
-keymap("n", "<leader>lpb", "<cmd>lua require('telescope.builtin').builtin()<cr>", opts)
-keymap("n", "<leader>lc", "<cmd>lua require('telescope.builtin').command_history()<cr>", opts)
-keymap("v", "<leader>lc", "<cmd>lua require('telescope.builtin').command_history()<cr>", opts)
-keymap("n", "<leader>ls", "<cmd>lua require('telescope.builtin').search_history()<cr>", opts)
-keymap("v", "<leader>ls", "<cmd>lua require('telescope.builtin').search_history()<cr>", opts)
-keymap("n", "<leader>lm", "<cmd>lua require('telescope.builtin').marks()<cr>", opts)
-keymap("n", "<leader>fnf", "<cmd>lua require('telescope.builtin').find_files{cwd = '~/.config/nvim'}<CR>", opts)
-keymap("n", "<leader>fb", "<cmd>lua require 'telescope'.extensions.file_browser.file_browser{hidden = true}<CR>", opts)
-keymap("n", "<leader>bl", "<cmd>lua require('telescope.builtin').buffers{show_all_buffers = true}<cr>", opts)
+local builtin = require("telescope.builtin")
+local file_browser = require "telescope".extensions.file_browser
+
+keymap(
+  "n",
+  "<leader>ff",
+  function()
+    builtin.find_files()
+  end,
+  opts
+)
+keymap(
+  "n",
+  "<leader>fgf",
+  function()
+    builtin.git_files()
+  end,
+  opts
+)
+keymap(
+  "n",
+  "<leader>fw",
+  function()
+    builtin.live_grep()
+  end,
+  opts
+)
+keymap(
+  "n",
+  "<leader>bfw",
+  function()
+    builtin.current_buffer_fuzzy_find()
+  end,
+  opts
+)
+keymap(
+  "n",
+  "<leader>*",
+  function()
+    builtin.grep_string()
+  end,
+  opts
+)
+keymap(
+  "n",
+  "<leader>**",
+  function()
+    builtin.grep_string({grep_open_files = true})
+  end,
+  opts
+)
+keymap(
+  "n",
+  "<leader>lb",
+  function()
+    builtin.buffers()
+  end,
+  opts
+)
+keymap(
+  "n",
+  "<leader>lj",
+  function()
+    builtin.jumplist()
+  end,
+  opts
+)
+keymap(
+  "n",
+  "<leader>lpb",
+  function()
+    builtin.builtin()
+  end,
+  opts
+)
+keymap(
+  "n",
+  "<leader>lc",
+  function()
+    builtin.command_history()
+  end,
+  opts
+)
+keymap(
+  "v",
+  "<leader>lc",
+  function()
+    builtin.command_history()
+  end,
+  opts
+)
+keymap(
+  "n",
+  "<leader>ls",
+  function()
+    builtin.search_history()
+  end,
+  opts
+)
+keymap(
+  "v",
+  "<leader>ls",
+  function()
+    builtin.search_history()
+  end,
+  opts
+)
+keymap(
+  "n",
+  "<leader>lm",
+  function()
+    builtin.marks()
+  end,
+  opts
+)
+keymap(
+  "n",
+  "<leader>fnf",
+  function()
+    builtin.find_files {cwd = "~/.config/nvim"}
+  end,
+  opts
+)
+keymap(
+  "n",
+  "<leader>fb",
+  function()
+    file_browser.file_browser {hidden = true}
+  end,
+  opts
+)
+keymap(
+  "n",
+  "<leader>bl",
+  function()
+    builtin.buffers {show_all_buffers = true}
+  end,
+  opts
+)
