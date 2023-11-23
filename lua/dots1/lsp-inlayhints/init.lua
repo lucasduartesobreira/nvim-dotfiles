deaaeb135614664b403fc4516a58544678520d50
Lucas Duarte Sobreira <lucasduartesobreira@gmail.com>
2023-11-22 21:11:44 -0300

    refactor(hints): use builtin inlay hints

--- lua/dots1/lsp-inlayhints/init.lua
+++ lua/dots1/lsp-inlayhints/init.lua
@@ -1,45 +1,10 @@
-local is_lsp_inlayhints_ok, lsp_inlayhints = pcall(require, "lsp-inlayhints")
-if not is_lsp_inlayhints_ok then
-  return
-end
-
-lsp_inlayhints.setup()
-
-local function mappings(bufnr)
-  local opts = {noremap = true, silent = true, buffer = bufnr}
-  local keymap = vim.keymap.set
-  keymap(
+if vim.lsp.inlay_hint then
+  vim.keymap.set(
     "n",
     "<leader>it",
     function()
-      lsp_inlayhints.toggle()
+      vim.lsp.inlay_hint.enable(0, not vim.lsp.inlay_hint.is_enabled(0))
     end,
-    opts
-  )
-  keymap(
-    "n",
-    "<leader>ir",
-    function()
-      lsp_inlayhints.reset()
-    end,
-    opts
+    {desc = "Toggle Inlay Hints"}
   )
 end
-
-vim.api.nvim_create_augroup("LspAttach_inlayhints", {})
-vim.api.nvim_create_autocmd(
-  "LspAttach",
-  {
-    group = "LspAttach_inlayhints",
-    callback = function(args)
-      if not (args.data and args.data.client_id) then
-        return
-      end
-
-      local bufnr = args.buf
-      local client = vim.lsp.get_client_by_id(args.data.client_id)
-      lsp_inlayhints.on_attach(client, bufnr)
-      mappings(bufnr)
-    end
-  }
-)
