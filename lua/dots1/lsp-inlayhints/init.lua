325a9950d91951c7bc2eb528ec410d4a5664c06f
Lucas Duarte Sobreira <lucasduartesobreira@gmail.com>
2024-08-23 16:02:10 -0300

    fix(inlayhints): toggle inlay hints bind

--- lua/dots1/lsp-inlayhints/init.lua
+++ lua/dots1/lsp-inlayhints/init.lua
@@ -3,7 +3,7 @@ if vim.lsp.inlay_hint then
     "n",
     "<leader>it",
     function()
-      vim.lsp.inlay_hint.enable(0, not vim.lsp.inlay_hint.is_enabled(0))
+      vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
     end,
     {desc = "Toggle Inlay Hints"}
   )
