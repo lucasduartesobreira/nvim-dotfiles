64ef49601d13395ef7647729fe45cc99f5207bf1
Lucas Duarte Sobreira <lucasduartesobreira@gmail.com>
2024-08-23 16:01:40 -0300

    fix(gitsigns): deprecated code

--- lua/dots1/gitsigns/init.lua
+++ lua/dots1/gitsigns/init.lua
@@ -7,11 +7,20 @@ local mappings = require("dots1.gitsigns.mappings")
 
 gitsigns.setup {
   signs = {
-    add = {hl = "GitSignsAdd", text = "│", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn"},
-    change = {hl = "GitSignsChange", text = "│", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn"},
-    delete = {hl = "GitSignsDelete", text = "_", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn"},
-    topdelete = {hl = "GitSignsDelete", text = "‾", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn"},
-    changedelete = {hl = "GitSignsChange", text = "~", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn"}
+    add = {text = "┃"},
+    change = {text = "┃"},
+    delete = {text = "_"},
+    topdelete = {text = "‾"},
+    changedelete = {text = "~"},
+    untracked = {text = "┆"}
+  },
+  signs_staged = {
+    add = {text = "┃"},
+    change = {text = "┃"},
+    delete = {text = "_"},
+    topdelete = {text = "‾"},
+    changedelete = {text = "~"},
+    untracked = {text = "┆"}
   },
   signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
   numhl = false, -- Toggle with `:Gitsigns toggle_numhl`
@@ -41,9 +50,6 @@ gitsigns.setup {
     relative = "cursor",
     row = 0,
     col = 1
-  },
-  yadm = {
-    enable = false
   },
   on_attach = mappings.on_attach
 }
