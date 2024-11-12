448ae3ba83cee451977e56d418d7f48e502f4b61
Lucas Duarte Sobreira <lucasduartesobreira@gmail.com>
2024-11-11 22:25:21 -0300

    fix(theme): change completely the theme when updating coloscheme

--- lua/dots1/cokeline/init.lua
+++ lua/dots1/cokeline/init.lua
@@ -3,133 +3,152 @@ if not is_cokeline_ok then
   return
 end
 
+local hlgroups_utils = require("cokeline.hlgroups")
+
 local is_picking_focus = require("cokeline.mappings").is_picking_focus
 local is_picking_close = require("cokeline.mappings").is_picking_close
-local get_hex = require("cokeline.hlgroups").get_hl_attr
-local errors_fg = get_hex("DiagnosticError", "fg")
-local warnings_fg = get_hex("DiagnosticWarn", "fg")
-local info_fg = get_hex("DiagnosticInfo", "fg")
-local hint_fg = get_hex("DiagnosticHint", "fg")
-local green = vim.g.terminal_color_2
-local coloscheme_name = vim.g.colors_name
-local palette = require("nightfox.palette").load(coloscheme_name)
-local red = vim.g.terminal_color_1
-local yellow = vim.g.terminal_color_3
+local get_hex = hlgroups_utils.get_hl_attr
+local augroup = vim.api.nvim_create_augroup
+local aucmd = vim.api.nvim_create_autocmd
+local terminal_id = augroup("Change Color Scheme", {clear = false})
 
-vim.api.nvim_set_hl(0, "TabLine", {bg = palette.bg0, fg = palette.fg2})
+local function create_config()
+  hlgroups_utils._cache_clear()
+  local errors_fg = get_hex("DiagnosticError", "fg")
+  local warnings_fg = get_hex("DiagnosticWarn", "fg")
+  local info_fg = get_hex("DiagnosticInfo", "fg")
+  local hint_fg = get_hex("DiagnosticHint", "fg")
+  local status_line_bg = get_hex("StatusLine", "bg")
+  local status_line_fg = get_hex("StatusLine", "fg")
+  local green = vim.g.terminal_color_2
+  local red = vim.g.terminal_color_1
+  local yellow = vim.g.terminal_color_3
 
-cokeline.setup(
-  {
-    default_hl = {
-      fg = function(buffer)
-        return buffer.is_focused and get_hex("Normal", "fg") or get_hex("Comment", "fg")
-      end,
-      bg = get_hex("ColorColumn", "bg")
-    },
-    components = {
-      {
-        text = " ",
-        bg = get_hex("TabLine", "bg")
-      },
-      {
-        text = "",
-        fg = get_hex("ColorColumn", "bg"),
-        bg = get_hex("TabLine", "bg")
-      },
-      {
-        text = function(buffer)
-          return (is_picking_focus() or is_picking_close()) and buffer.pick_letter .. " " or buffer.devicon.icon
-        end,
+  vim.api.nvim_set_hl(0, "TabLine", {bg = status_line_bg, fg = status_line_fg})
+  cokeline.setup(
+    {
+      default_hl = {
         fg = function(buffer)
-          return (is_picking_focus() and yellow) or (is_picking_close() and red) or buffer.devicon.color
+          return buffer.is_focused and get_hex("Normal", "fg") or get_hex("Comment", "fg")
         end,
-        style = function(_)
-          return (is_picking_focus() or is_picking_close()) and "italic,bold" or nil
-        end
+        bg = get_hex("ColorColumn", "bg")
       },
-      {
-        text = " "
-      },
-      {
-        text = function(buffer)
-          local has_diagnostics =
-            buffer.diagnostics.errors ~= 0 or buffer.diagnostics.warnings ~= 0 or buffer.diagnostics.infos ~= 0 or
-            buffer.diagnostics.hints ~= 0
+      components = {
+        {
+          text = " ",
+          bg = get_hex("TabLine", "bg")
+        },
+        {
+          text = "",
+          fg = get_hex("ColorColumn", "bg"),
+          bg = get_hex("TabLine", "bg")
+        },
+        {
+          text = function(buffer)
+            return (is_picking_focus() or is_picking_close()) and buffer.pick_letter .. " " or buffer.devicon.icon
+          end,
+          fg = function(buffer)
+            return (is_picking_focus() and yellow) or (is_picking_close() and red) or buffer.devicon.color
+          end,
+          style = function(_)
+            return (is_picking_focus() or is_picking_close()) and "italic,bold" or nil
+          end
+        },
+        {
+          text = " "
+        },
+        {
+          text = function(buffer)
+            local has_diagnostics =
+              buffer.diagnostics.errors ~= 0 or buffer.diagnostics.warnings ~= 0 or buffer.diagnostics.infos ~= 0 or
+              buffer.diagnostics.hints ~= 0
 
-          local space_after_sign = has_diagnostics and "" or "  "
-          return (buffer.unique_prefix) .. buffer.filename .. space_after_sign
-        end,
-        style = function(buffer)
-          return buffer.is_focused and "bold" or nil
-        end
-      },
-      {
-        text = function(buffer)
-          local errors = buffer.diagnostics.errors ~= 0 and "  " .. buffer.diagnostics.errors or ""
+            local space_after_sign = has_diagnostics and "" or "  "
+            return (buffer.unique_prefix) .. buffer.filename .. space_after_sign
+          end,
+          style = function(buffer)
+            return buffer.is_focused and "bold" or nil
+          end
+        },
+        {
+          text = function(buffer)
+            local errors = buffer.diagnostics.errors ~= 0 and "  " .. buffer.diagnostics.errors or ""
 
-          return errors
-        end,
-        fg = function(buffer)
-          return buffer.diagnostics.errors ~= 0 and errors_fg or nil
-        end,
-        truncation = {priority = 1}
-      },
-      {
-        text = function(buffer)
-          local warnings = buffer.diagnostics.warnings ~= 0 and "  " .. buffer.diagnostics.warnings or ""
+            return errors
+          end,
+          fg = function(buffer)
+            return buffer.diagnostics.errors ~= 0 and errors_fg or nil
+          end,
+          truncation = {priority = 1}
+        },
+        {
+          text = function(buffer)
+            local warnings = buffer.diagnostics.warnings ~= 0 and "  " .. buffer.diagnostics.warnings or ""
 
-          return warnings
-        end,
-        fg = function(buffer)
-          return buffer.diagnostics.warnings ~= 0 and warnings_fg or nil
-        end,
-        truncation = {priority = 100}
-      },
-      {
-        text = function(buffer)
-          local infos = buffer.diagnostics.infos ~= 0 and "  " .. buffer.diagnostics.infos or ""
+            return warnings
+          end,
+          fg = function(buffer)
+            return buffer.diagnostics.warnings ~= 0 and warnings_fg or nil
+          end,
+          truncation = {priority = 100}
+        },
+        {
+          text = function(buffer)
+            local infos = buffer.diagnostics.infos ~= 0 and "  " .. buffer.diagnostics.infos or ""
 
-          return infos
-        end,
-        fg = function(buffer)
-          return buffer.diagnostics.infos ~= 0 and info_fg or nil
-        end,
-        truncation = {priority = 100}
-      },
-      {
-        text = function(buffer)
-          local hints = buffer.diagnostics.hints ~= 0 and "  " .. buffer.diagnostics.hints or ""
+            return infos
+          end,
+          fg = function(buffer)
+            return buffer.diagnostics.infos ~= 0 and info_fg or nil
+          end,
+          truncation = {priority = 100}
+        },
+        {
+          text = function(buffer)
+            local hints = buffer.diagnostics.hints ~= 0 and "  " .. buffer.diagnostics.hints or ""
 
-          return hints
-        end,
-        fg = function(buffer)
-          return buffer.diagnostics.hints ~= 0 and hint_fg or nil
-        end,
-        truncation = {priority = 100}
-      },
-      {
-        text = function(buffer)
-          local has_diagnostics =
-            buffer.diagnostics.errors ~= 0 or buffer.diagnostics.warnings ~= 0 or buffer.diagnostics.infos ~= 0 or
-            buffer.diagnostics.hints ~= 0
+            return hints
+          end,
+          fg = function(buffer)
+            return buffer.diagnostics.hints ~= 0 and hint_fg or nil
+          end,
+          truncation = {priority = 100}
+        },
+        {
+          text = function(buffer)
+            local has_diagnostics =
+              buffer.diagnostics.errors ~= 0 or buffer.diagnostics.warnings ~= 0 or buffer.diagnostics.infos ~= 0 or
+              buffer.diagnostics.hints ~= 0
 
-          local space_before_sign = has_diagnostics and " " or ""
-          return space_before_sign .. (buffer.is_modified and "●" or "x")
-        end,
-        fg = function(buffer)
-          return buffer.is_modified and green or nil
-        end,
-        delete_buffer_on_left_click = true,
-        truncation = {priority = 1}
-      },
-      {
-        text = "",
-        fg = get_hex("ColorColumn", "bg"),
-        bg = get_hex("TabLine", "bg")
+            local space_before_sign = has_diagnostics and " " or ""
+            return space_before_sign .. (buffer.is_modified and "●" or "x")
+          end,
+          fg = function(buffer)
+            return buffer.is_modified and green or nil
+          end,
+          delete_buffer_on_left_click = true,
+          truncation = {priority = 1}
+        },
+        {
+          text = "",
+          fg = get_hex("ColorColumn", "bg"),
+          bg = get_hex("TabLine", "bg")
+        }
       }
     }
+  )
+end
+
+aucmd(
+  {"ColorScheme"},
+  {
+    group = terminal_id,
+    pattern = "*",
+    callback = create_config
   }
 )
+
+create_config()
 
 local keymap = vim.keymap.set
 keymap("n", "<leader>bd", ":bdelete<CR>", {silent = true})
--- lua/dots1/lualine/init.lua
+++ lua/dots1/lualine/init.lua
@@ -3,7 +3,48 @@ if not is_lualine_ok then
   return
 end
 
+local augroup = vim.api.nvim_create_augroup
+local aucmd = vim.api.nvim_create_autocmd
+local terminal_id = augroup("Change Color Scheme", {clear = false})
 local colorscheme_name = vim.g.colors_name
+
+aucmd(
+  {"ColorScheme"},
+  {
+    group = terminal_id,
+    pattern = "*",
+    callback = function()
+      colorscheme_name = vim.g.colors_name
+      lualine.setup {
+        options = {
+          icons_enabled = true,
+          theme = colorscheme_name,
+          disabled_filetypes = {},
+          section_separators = "",
+          component_separators = ""
+        },
+        sections = {
+          lualine_a = {"mode"},
+          lualine_b = {"branch", "diff"},
+          lualine_c = {"filename"},
+          lualine_x = {"filetype"},
+          lualine_y = {"progress"},
+          lualine_z = {"location"}
+        },
+        inactive_sections = {
+          lualine_a = {},
+          lualine_b = {},
+          lualine_c = {"filename"},
+          lualine_x = {"location"},
+          lualine_y = {},
+          lualine_z = {}
+        },
+        tabline = {},
+        extensions = {}
+      }
+    end
+  }
+)
 
 lualine.setup {
   options = {
