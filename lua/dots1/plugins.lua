791474f331d590ea84d4481047ada5b997017c04
lucas_cargon <lucas@cargon.com.br>
2025-02-08 20:53:39 -0300

    chore(neotest): add missing dep

--- lua/dots1/plugins.lua
+++ lua/dots1/plugins.lua
@@ -151,10 +151,12 @@ return packer.startup(
     use {
       "nvim-neotest/neotest",
       requires = {
+        "nvim-neotest/nvim-nio",
         "nvim-lua/plenary.nvim",
         "nvim-treesitter/nvim-treesitter",
         "antoinemadec/FixCursorHold.nvim",
         "haydenmeade/neotest-jest",
+        "marilari88/neotest-vitest",
         "rouge8/neotest-rust"
       }
     }
