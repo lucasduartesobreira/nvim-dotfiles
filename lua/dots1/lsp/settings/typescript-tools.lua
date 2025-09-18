9c9ffd61cd830bf94dea340dbd059feaf500c14a
lucas_cargon <lucas@cargon.com.br>
2025-09-17 23:32:31 -0300

    refactor(lsp): some changes on typescript-tools config

--- lua/dots1/lsp/settings/typescript-tools.lua
+++ lua/dots1/lsp/settings/typescript-tools.lua
@@ -1,6 +1,33 @@
+vim.api.nvim_create_user_command(
+  "LspStop",
+  function(info)
+    local clients = info.fargs
+    if #clients == 0 then
+      clients =
+        vim.iter(vim.lsp.get_clients({bufnr = vim.api.nvim_get_current_buf()})):map(
+        function(client)
+          return client.name
+        end
+      ):totable()
+    end
+    for _, name in ipairs(clients) do
+      if vim.lsp.config[name] == nil then
+        vim.notify(("Invalid server name '%s'"):format(name))
+      else
+        vim.lsp.enable(name, false)
+      end
+    end
+  end,
+  {
+    desc = "Disable and stop the given client",
+    nargs = "?"
+  }
+)
+
 local localSettings = {
   settings = {
     separate_diagnostic_server = true,
+    tsserver_max_memory = 6192,
     tsserver_file_preferences = {
       includeCompletionsForModuleExports = true,
       includeInlayParameterNameHints = "all",
