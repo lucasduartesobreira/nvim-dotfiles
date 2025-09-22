d19b5f57618b4e11399db09c42b3877421754bf7
Lucas Duarte Sobreira <lucasduartesobreira@gmail.com>
2025-09-22 20:44:33 -0300

    fix(lsp): using deprecated way of setting Lsp diagnostic signs

--- lua/dots1/lsp/handlers.lua
+++ lua/dots1/lsp/handlers.lua
@@ -68,24 +68,18 @@ end
 local M = {}
 
 M.setup = function()
-  local signs = {
-    {name = "DiagnosticSignError", text = ""},
-    {name = "DiagnosticSignWarn", text = ""},
-    {name = "DiagnosticSignHint", text = ""},
-    {name = "DiagnosticSignInfo", text = ""}
-  }
-
-  for _, sign in ipairs(signs) do
-    vim.fn.sign_define(sign.name, {texthl = sign.name, text = sign.text, numhl = ""})
-  end
-
   local config = {
     -- disable virtual text
     virtual_text = false,
     virtual_lines = build_diagnostic_virtual_lines_config(false),
     -- show signs
     signs = {
-      active = signs
+      text = {
+        [vim.diagnostic.severity.ERROR] = "",
+        [vim.diagnostic.severity.WARN] = "",
+        [vim.diagnostic.severity.INFO] = "",
+        [vim.diagnostic.severity.HINT] = ""
+      }
     },
     update_in_insert = true,
     underline = true,
